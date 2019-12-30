ea51b815ab53586bf01454611c705c68f7ef645c
Zoooook <zzzzzzooooook@gmail.com>
2019-12-30 16:11:19 -0700

    exclude more upgrades

--- CookieClickerBot.js
+++ CookieClickerBot.js
@@ -445,7 +445,22 @@ function calculateBestThing(){
 
     for (var i in Game.UpgradesInStore) {
         var me = Game.UpgradesInStore[i];
-        if (!['One mind', 'Golden switch [off]'].includes(me.name)) {
+        var excluded_upgrades = [
+            'One mind',
+            'Elder Covenant',
+            'Elder Pledge',
+            'Golden switch [off]',
+            'Bunny biscuit',
+            'Festive biscuit',
+            'Fool\'s biscuit',
+            'Ghostly biscuit',
+            'Lovesick biscuit',
+            'Sugar frenzy',
+            'Milk selector',
+            'Background selector',
+            'Golden cookie sound selector',
+        ];
+        if (!excluded_upgrades.includes(me.name)) {
             args[me.name] = ['', 0, me.name, 0, 0];
             things[me.name] = {type: 'upgrade', name: me.name, cps: calculateCps(...args[me.name])};
             things[me.name].percent = (things[me.name].cps / currentCps - 1) * 100;
